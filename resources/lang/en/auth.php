f446f0a6fec96a5370e584d82fc18be7f34c2392
sameep09 <sameeppoudel71@gmail.com>
2025-02-19 12:48:48 +0545

    Add files via upload

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'प्रयोगकर्ताको इ - मेल ठेगाना वा पासवर्ड मिलेन।',
+    'password' => 'प्रदान गरिएको पासवर्ड गलत छ।',
+    'throttle' => 'धेरै लगईन प्रयासहरू। कृपया पुन: केहि सेकेन्ड पछि प्रयास गर्नुहोस्।',
+
+];
